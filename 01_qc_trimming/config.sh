41e780b3ef7562ae471a8e0834f34c3342140d8a
Mery Touceda-Suárez <maria.touceda.suarez@gmail.com>
2025-10-13 14:49:48 -0400

    harmonized, untested

--- 01_qc_trimming/config.sh
+++ 01_qc_trimming/config.sh
@@ -1,5 +1,5 @@
 export ID=MY_ID
-export ACCESSIONS=MY_ACCESSIONS
+export IN_LIST=MY_ACCESSIONS
 export FASTQC=/path_to_containers/fastqc-0.11.9.sif
 export TRIMMOMATIC=/path_to_containers/trimmomatic:0.39--hdfd78af_2.sif
 export WORK_DIR=/my_dir_path/MY_ID/01_qc_trimming
