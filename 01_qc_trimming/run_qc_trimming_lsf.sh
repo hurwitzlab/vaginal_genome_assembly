265a4510736dbd0baaf0711d3dd2886b2a1dff99
Mery Touceda-Suárez <maria.touceda.suarez@gmail.com>
2025-10-13 15:12:26 -0400

    launchers, harmonized, untested

--- 01_qc_trimming/run_qc_trimming_lsf.sh
+++ 01_qc_trimming/run_qc_trimming_lsf.sh
@@ -1,15 +1,40 @@
 #! /bin/bash
+# This script submits a series of job arrays to an LSF scheduler
+# Each job array corresponds to a step in the QC and trimming process
+# The steps are:
+# 1. FastQC on raw reads
+# 2. Trimmomatic for trimming reads
+# 3. FastQC on trimmed reads    
+# Each job array will process multiple samples in parallel
+# The number of samples is determined by the number of lines in the input list file     
+# --------------------------------------------------
 
-export JOB1="01A_run_fastqc-lsf"
-export JOB2="01B_run_trimmomatic-lsf"
-export JOB3="01C_run_fastqc-lsf"
 
-# JOB1: first job - no dependencies
-bsub -J $JOB1 < ${JOB1}.sh
+# load job configuration
+source ./config.sh
 
-# JOB2 depends on JOB1
-bsub -J $JOB2 -w 'done($JOB1)' < ${JOB2}.sh
 
-# JOB3 depends on JOB2
-bsub -J $JOB3 -w 'done($JOB2)' < ${JOB3}.sh
+# make sure sample file is in the right place
+if [[ ! -f "$IN_LIST" ]]; then
+    echo "$IN_LIST does not exist. Please provide the path for a list of datasets to process. Job terminated."
+    exit 1
+fi
 
+export JOB1="fastqc"
+export JOB2="trimmomatic"
+export JOB3="fastqc_trimmed"
+
+# get number of samples to process
+# the number of samples will be used to set the range of the job array
+export NUM_JOB=$(wc -l < "$IN_LIST")
+
+# submit job arrays for each step
+echo "launching ${JOB1}.lsf as a job."
+JOB_ID=`bsub -J "$JOB1[1-$NUM_JOB]" < ${JOB1}.lsf`
+
+echo "launching ${JOB2}.lsf as a job."
+JOB_ID=`bsub -J "$JOB2[1-$NUM_JOB]" -w 'done($JOB1)' < ${JOB2}.lsf`
+
+
+echo "launching ${JOB3}.lsf as a job."
+JOB_ID=`bsub -J "$JOB3[1-$NUM_JOB]" -w 'done($JOB2)' < ${JOB3}.lsf`
--- 01_qc_trimming/run_qc_trimming_slurm.sh
+++ 01_qc_trimming/run_qc_trimming_slurm.sh
@@ -1,17 +1,47 @@
 #! /bin/bash
+# This script submits a series of job arrays to an LSF scheduler
+# Each job array corresponds to a step in the QC and trimming process
+# The steps are:
+# 1. FastQC on raw reads
+# 2. Trimmomatic for trimming reads
+# 3. FastQC on trimmed reads    
+# Each job array will process multiple samples in parallel
+# The number of samples is determined by the number of lines in the input list file     
+# --------------------------------------------------
 
-# 01A_run_fastqc: first job - no dependencies
-job1=$(sbatch 01A_run_fastqc.sh)
-jid1=$(echo $job1 | sed 's/^Submitted batch job //')
+
+# load job configuration
+source ./config.sh
+
+
+# make sure sample file is in the right place
+if [[ ! -f "$IN_LIST" ]]; then
+    echo "$IN_LIST does not exist. Please provide the path for a list of datasets to process. Job terminated."
+    exit 1
+fi
+
+export JOB1="fastqc"
+export JOB2="trimmomatic"
+export JOB3="fastqc_trimmed"
+
+# get number of samples to process
+# the number of samples will be used to set the range of the job array
+export NUM_JOB=$(wc -l < "$IN_LIST")
+
+# submit job arrays for each step
+echo "launching ${JOB1}.slurm as a job."
+JOB_ID=`sbatch --job-name $JOB1 -a 1-$NUM_JOB ${JOB1}.slurm`
+jid1=$(echo $JOB1 | sed 's/^Submitted batch job //')
 echo $jid1
 
-# 01B_run_trimmomatic: jid2 depends on jid1
-job2=$(sbatch --dependency=afterok:$jid1 01B_run_trimmomatic.sh)
-jid2=$(echo $job2 | sed 's/^Submitted batch job //')
+echo "launching ${JOB2}.slurm as a job."
+JOB_ID=`sbatch --dependency=afterok:$jid1 --job-name $JOB2 -a 1-$NUM_JOB ${JOB2}.slurm`     # depends on successful completion of JOB1
+jid2=$(echo $JOB2 | sed 's/^Submitted batch job //')
 echo $jid2
 
-# 01C_run_fastqc: jid3 depends on jid2
-job3=$(sbatch --dependency=afterok:$jid2 01C_run_fastqc.sh)
-jid3=$(echo $job3 | sed 's/^Submitted batch job //')
+echo "launching ${JOB3}.slurm as a job."
+JOB_ID=`sbatch --dependency=afterok:$jid2 --job-name $JOB3 -a 1-$NUM_JOB ${JOB3}.slurm`     # depends on successful completion of JOB2
+jid3=$(echo $JOB3 | sed 's/^Submitted batch job //')
 echo $jid3
+
 
